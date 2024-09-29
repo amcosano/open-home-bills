2f2a243d92c4dc2cd5582c27babcc4f18f77ea5c
amcosano <antoniocossano@gmail.com>
2024-09-29 23:53:14 +0200

    Added first approach header

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import { Box, Paper, Typography } from '@mui/material'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#3f51b5',
+          p: 2,
+          height: '80px',
+        }}
+      >
+        <Typography variant='h4' sx={{ color: '#e8eaf6' }}>
+          Open Home Bills
+        </Typography>
+        <Paper />
+      </Box>
     </>
   )
 }
