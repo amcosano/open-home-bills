a7087315f99d9900cc77aaabf6bcb5d81ef30e11
amcosano <antoniocossano@gmail.com>
2024-09-30 00:05:34 +0200

    Added app bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { Box, Paper, Typography } from '@mui/material'
+import AppBarComponent from './components/appbar/AppBarComponent'
 
 const App = () => {
   return (
     <>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor: '#3f51b5',
-          p: 2,
-          height: '80px',
-        }}
-      >
-        <Typography variant='h4' sx={{ color: '#e8eaf6' }}>
-          Open Home Bills
-        </Typography>
-        <Paper />
-      </Box>
+      <AppBarComponent />
     </>
   )
 }
---
+++ src/components/appbar/AppBarComponent.jsx
@@ -0,0 +1,22 @@
+import { Box, Paper, Typography } from '@mui/material'
+
+const AppBarComponent = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#3f51b5',
+        p: 2,
+        height: '80px',
+      }}
+    >
+      <Typography variant='h4' sx={{ color: '#e8eaf6' }}>
+        Open Home Bills
+      </Typography>
+      <Paper />
+    </Box>
+  )
+}
+
+export default AppBarComponent
